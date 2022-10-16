7030c34cdf3cfb18e5292e74a3ca07ae3549f89b
Kauan <98900217+Kauan3323@users.noreply.github.com>
2022-10-16 16:13:02 -0300

    Update CoolUtil.hx

--- source/meta/CoolUtil.hx
+++ source/meta/CoolUtil.hx
@@ -5,7 +5,7 @@ import meta.state.PlayState;
 
 using StringTools;
 
-#if !html5
+#if !android
 import sys.FileSystem;
 #end
 
@@ -64,7 +64,7 @@ class CoolUtil
 	{
 		//
 		var libraryArray:Array<String> = [];
-		#if !html5
+		#if !android
 		var unfilteredLibrary = FileSystem.readDirectory('$subDir/$library');
 
 		for (folder in unfilteredLibrary)
